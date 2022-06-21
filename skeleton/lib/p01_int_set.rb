53de347004e5f222492140e96e38c0922eeca9b9
Andy Liu <andyliu1527@gmail.com>
2022-06-21 00:22:21 -0400

    first commit

---
+++ skeleton/lib/p01_int_set.rb
@@ -0,0 +1,112 @@
+class MaxIntSet
+  attr_reader :store
+  def initialize(max)
+    @max = max
+    @store = Array.new(max, false)
+  end
+
+  def insert(num)
+    if is_valid?(num)
+      @store[num] = true
+    else
+      raise "Out of bounds"
+    end
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    return @store[num]
+  end
+
+  private
+
+  def is_valid?(num)
+    return false if num > @max || num < 0
+    return true
+  end
+
+  def validate!(num)
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    @store[num % num_buckets] << num
+    return true
+  end
+
+  def remove(num)
+    @store[num % num_buckets].delete(num)
+  end
+
+  def include?(num)
+    @store[num % num_buckets].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if count == num_buckets
+    if !self[num].include?(num)
+      @count += 1
+      self[num] << num
+    end
+  end
+
+  def remove(num)
+    if self[num].include?(num)
+      @count -= 1
+      self[num].delete(num)
+    end
+  end
+
+  def include?(num)
+    return true if self[num].include?(num)
+    return false
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    # Array.new(num_buckets) {Array.new}
+    @count = 0
+    store_dup = @store.dup.flatten
+    @store = (Array.new(num_buckets * 2) {Array.new})
+    
+    store_dup.each {|num| self.insert(num)}
+  end
+end
